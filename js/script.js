839a2e8c1abad6ad6e231b180d42c7aed4b9339c
David Fan <dfan14051@gmail.com>
2017-02-09 15:31:09 -0500

    Added table print out

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-let template = "<div class='container'><form id = 'matrix-create' action='#'><div class='center'>Rows: <input class='size' type='text' value='0'>Columns: <input class='size' type='text' value='0'><input class='submit' onclick='createMatrix();' type='button' value='Set Matrix Size'> </div> </form> </div> <form id='matrix-form' action='#'> <div class='table-container center inline'> <table class='left' id='matrix'> </table> <table class ='right' id='solution'> </table> </div> <div id='submit-button'> </div> </form>";
+let template = "<div class='container'><form id = 'matrix-create' action='#'><div class='center'>" +
+"Rows: <input class='size' type='text' value='0'>Columns: <input class='size' type='text' " +
+"value='0'><input class='submit' onclick='createMatrix();' type='button' value='Set Matrix " +
+"Size'> </div> </form> </div> <form id='matrix-form' action='#'> <div class='table-container " +
+"center inline'> <table class='left' id='matrix'> </table> <table class ='right' id='solution'> " +
+"</table> </div> <div id='submit-button'> </div> </form>";
 $(document).ready(function () {
   homeScreen();}
 );
@@ -60,31 +65,124 @@ function createMatrix() {
 }
 
 function simplify(height, width) {
+  //load the matrix
   let $mrows = $('.mrow');
   let $srows = $('.srow');
   let matrix = [];
   for (let i = 0; i < height; i++) {
     matrix[i] = [];
     for (let j = 0; j < width - 1; j++) {
-      console.log($mrows.get(i).childNodes[j].childNodes[0].value);
       matrix[i][j] = $mrows.get(i).childNodes[j].childNodes[0].value;
     }
 
     matrix[i][width - 1] = $srows.get(i).childNodes[0].childNodes[0].value;
   }
 
-  //pivot
-  //pivot index
-  for (let i = 0; i < height; i++) {
-  	//simplification steps
-  }
-
-  console.log(matrix);
-
+  //empty the page
   let $content = $('.content');
   $content.empty();
 
+  //actual simplification algorithm
+  let pivot = 0;
+  let pivotIndex = 0;
+  while (pivot < height || pivotIndex < width) {
+    let oneFound = false;
+    let emptyColumn = true;
+
+    //See if any pivot is already a 1
+    for (let i = pivot; i < height; i++) {
+      if (parseFloat(matrix[pivot][pivotIndex]) === 1) {
+        swapRow(pivot, i, matrix);
+        oneFound = true;
+        emptyColumn = false;
+      }
+    }
+
+    if (!oneFound) {
+      //find the first nonzero pivot
+      let nonZero = pivot;
+      while (nonZero < height && parseFloat(matrix[nonZero][pivotIndex]) !== 0) {
+        nonZero++;
+      }
+
+      //If no nonzero pivot
+      if (nonZero === height) {
+        pivotIndex++;
+      } else {
+        emptyColumn = false;
+        if (nonZero != pivot) {
+          swapRow(pivot, nonzero, matrix);
+        }
+
+        multiplyConstant(pivot, 1 / matrix[pivot][pivotIndex], matrix);
+
+      }
+
+    }
+
+    if (!emptyColumn) {
+      pivot++;
+    }
+  }
+
+  //add a button to go back to matrix creation
   let button = "<button class='submit' onclick='homeScreen();'>New Matrix</button>";
   $content.append(button);
 
 }
+
+/*
+  =================
+  matrix operations
+  =================
+*/
+
+// R1 <=> R2
+function swapRow(row1, row2, matrix) {
+  let tempRow = matrix[row1];
+  matrix[row1] = matrix[row2];
+  matrix[row2] = tempRow;
+  message = 'Swap Row ' + row1 + ' with Row ' + row2;
+  printOperation(message, matrix);
+}
+
+// R = cR
+function multiplyConstant(row, scalar, matrix) {
+  for (let i = 0; i < matrix[row].length; i++) {
+    matrix[row][i] = scalar * matrix[row][i];
+  }
+
+  message = 'Multiply Row ' + row1 + ' by ' + scalar;
+  printOperation(message, matrix);
+}
+
+// R1 = R1 + cR2
+function addRowtoRow(row1, row2, scalar, matrix) {
+  for (let i = 0; i < matrix[row1].length; i++) {
+    matrix[row1][i] = parseFloat(matrix[row1][i]) + scalar * matrix[row2][i];
+  }
+
+  message = 'Add ' + scalar + ' times Row ' + row2 + 'to Row ' + row1;
+  printOperation(message, matrix);
+}
+
+// Print out the current operation and what the matrix looks like now
+function printOperation(message, matrix) {
+  let operationMessage = "<div class = 'center'><p>" + message + '</p></div>';
+  let table = '<table>';
+  for (let i = 0; i < matrix.length; i++) {
+    table += '<tr>';
+    for (let j = 0; j < matrix[i].length; j++) {
+      table += '<td>' + matrix[i][j] + '</td>';
+    }
+
+    table += '</tr>';
+  }
+  let $content = $('.content');
+  $content.append(operationMessage);
+  $content.append(table);
+
+
+
+}
+
